595ceaee828415e5cafb77ab241e5febf4945ff8
pilar <pilar@DESKTOP-48AVSD1>
2024-01-04 21:07:07 -0700

    Commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,14 @@
+package d;
+
+public class DemoD {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Thursday");
+		
+		System.out.println("Sprint 7 work...");
+		System.out.println("Sprint 8 work...");
+
+}
+}
